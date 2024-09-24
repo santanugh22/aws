103789d34adf1b791339a1cf31a4fe3d99593840
santanugh22 <11463046+santanugh22@users.noreply.github.com>
2024-09-24 09:46:52 +0530

    Meow 4

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World meow this is the second test");
+  res.sendFile(__dirname + "/index.html");
 
 });
 
